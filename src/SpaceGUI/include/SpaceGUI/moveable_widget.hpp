a94cbfc2e7c63ef018a435acb1c2ea1b57fd48dd
cameron <ckiw23@gmail.com>
2025-11-20 23:21:34 -0500

    you can now resize widgets

--- src/SpaceGUI/include/SpaceGUI/moveable_widget.hpp
+++ src/SpaceGUI/include/SpaceGUI/moveable_widget.hpp
@@ -12,7 +12,24 @@ public:
 protected:
     void mousePressEvent(QMouseEvent *event) override;
     void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void leaveEvent(QEvent *event) override;
 
 private:
+private:
+    enum ResizeRegion {
+        NONE,
+        LEFT, RIGHT, TOP, BOTTOM,
+        TOPLEFT, TOPRIGHT, BOTTOMLEFT, BOTTOMRIGHT
+    };
+
+    ResizeRegion detectRegion(const QPoint &pos);
+    void updateCursor(const ResizeRegion region);
+
     QPoint dragStartPos_;
+    QRect initialGeometry_;
+    ResizeRegion resizeRegion_ = NONE;
+    const int borderWidth_ = 16;   // Resize margin around edges
+
+
 };
